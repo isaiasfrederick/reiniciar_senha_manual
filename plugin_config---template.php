15191022e8ad77e3cd8e70493a6c642835fa3c2b
Isaias Frederick <isaiasfrederick@gmail.com>
2024-07-22 12:56:06 -0300

    2o commit (colocando configuracoes)

---
+++ plugin_config---template.php
@@ -0,0 +1,12 @@
+<?php
+
+global $CFG;
+
+// URL do seu Moodle
+$moodle_url = 'https://<url>/webservice/rest/server.php';
+
+//if (/*strpos($CFG->wwwroot, 'ead.uftm') !== false*/ true) {
+$token = '<token>';
+$wwwroot = 'https://<url>/';
+$curso_permitido = '<id_curso>';
+//}
